aeab1850615e93f6f94e92f7d76f184733478f9f
Kazazi Mohamad <mohammad.kazazi@gmail.com>
2023-12-08 13:25:58 -0800

    Test publish

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {GameComponent} from "./components/game/game.component";
-
-export const routes: Routes = [
-  {
-    path: 'play/:id',
-    component: GameComponent
-  }
-];
